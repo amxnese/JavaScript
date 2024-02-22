2bf9b691b222866ded5c107f2d7bced18f166b6c
amine <aminesdrt42@gmail.com>
2024-02-22 11:27:20 +0100

    Added 'variables.js' File

---
+++ variables.js
@@ -0,0 +1,11 @@
+// Declaring a Variable
+var name;
+
+// Assigning a Value To a Variable
+name = "amine";
+
+// The id of Any Tag Can Be Dealt With As a Variable
+console.log(hello) // <div id="hello">Hello From HTML</div>
+
+// Changing The Content of The Tag Which Has The id:hello
+hello.innerHTML = "Hello From JS"
