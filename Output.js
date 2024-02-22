6f0eac6248bad5dadd3bcfe9408efb17c34a31bc
amine <aminesdrt42@gmail.com>
2024-02-22 11:25:52 +0100

    Added 'Output.js' and 'index.html' Files

---
+++ Output.js
@@ -0,0 +1,17 @@
+// Sending an Alert To The User
+window.alert("This is Alert From Output File");
+
+// Writing HTML Code With JS
+document.write("<h1> This is Header From JS file");
+
+// Printing an Output On The Console
+console.log("Hello From Console")
+
+// Sending an Error To The Console
+console.error("Error")
+
+// Printing a Table On The Console
+console.table(["amine", "mohamed", "james"]);
+
+// Printing a Colored Output On The Console
+console.log("This is %cBlue%c and This is %cRed", "color: blue;font-size: 20px", " ", "color:red; font-size:20px")
