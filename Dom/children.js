48bdbb090a99c62ed75daadf478096f7f7e5dbee
amine <aminesdrt42@gmail.com>
2024-03-01 22:23:15 +0100

    Added 'children.js' file

---
+++ Dom/children.js
@@ -0,0 +1,13 @@
+let ele = document.querySelector("div");
+// Getting all Elements Within an Element
+console.log(ele.children);
+// Getting all Nodes Within an Element
+console.log(ele.childNodes);
+// Getting The First Node of an Element
+console.log(ele.firstChild);
+// Getting The Last Node of an Element
+console.log(ele.lastChild);
+// Getting The First Element of an Element
+console.log(ele.firstElementChild);
+// Getting The Last Element of an Element
+console.log(ele.lastElementChild);
--- Dom/product.js
+++ Dom/product.js
@@ -1,38 +1,39 @@
-// Creating a div
-myElement = document.createElement("div");
-myAttribute = document.createAttribute("data");
-myText = document.createTextNode("My Product");
-myComment = document.createComment("This is Div");
+for (let i = 0; i < 5; i++) {
+  // Creating a div
+  myElement = document.createElement("div");
+  myAttribute = document.createAttribute("data");
+  myComment = document.createComment("This is Div");
 
-myElement.className = "product";
-myElement.setAttributeNode(myAttribute);
-myElement.setAttribute("class", "product");
+  myElement.className = "product";
+  myElement.setAttributeNode(myAttribute);
+  myElement.setAttribute("class", "product");
 
-// Appending Text To Element
-myElement.appendChild(myText);
+  // Appending Comment
+  myElement.appendChild(myComment);
 
-// Appending Comment
-myElement.appendChild(myComment);
+  // Creating a Header
+  myHeader = document.createElement("h3");
+  headerTxt = document.createTextNode(`Product ${i + 1}`);
 
-// Creating a Header
-myHeader = document.createElement("h3");
-headerTxt = document.createTextNode("This is Header");
+  // Appending text to Header
+  myHeader.appendChild(headerTxt);
+  myHeader.style.color = "red";
+  myHeader.style.textDecoration = "underline";
 
-// Appending text to Header
-myHeader.appendChild(headerTxt);
+  // Creating a paragraph
+  myPar = document.createElement("p");
+  parTxt = document.createTextNode(`Product Number ${i + 1} Description`);
 
-// Creating a paragraph
-myPar = document.createElement("p");
-parTxt = document.createTextNode("This is Paragraph");
+  // Appending text to paragraph
+  myPar.appendChild(parTxt);
+  myPar.style.color = "blue";
 
-// Appending text to paragraph
-myPar.appendChild(parTxt);
+  // Appending Header To div
+  myElement.appendChild(myHeader);
 
-// Appending Header To div
-myElement.appendChild(myHeader);
+  // Appending paragraph To div
+  myElement.appendChild(myPar);
 
-// Appending paragraph To div
-myElement.appendChild(myPar);
-
-// Appending The Element to The body
-document.body.appendChild(myElement);
+  // Appending The Element to The body
+  document.body.appendChild(myElement);
+}
