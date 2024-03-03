eeea33e11d720ee0c24961692e2eb88cd27a4da5
amine <aminesdrt42@gmail.com>
2024-03-03 22:48:01 +0100

    Modified 'eventListener.js' File

--- Dom/eventListener.js
+++ Dom/eventListener.js
@@ -1,17 +1,39 @@
+// Getting The Original Header
 original = document.querySelector("h4");
 
+// Adding an EventListener to The Original Header
 original.addEventListener("click", function () {
+  // Cloning The Original Header
   let copy = original.cloneNode(true);
+  // Setting a ClassName To The Copy Element
   copy.className = "clone";
+  // Appending The Copy Element To The Body
   document.body.appendChild(copy);
 });
 
+// Adding Another EventListener To The Original Header
 original.addEventListener("click", function (event) {
   console.log(event.target);
 });
 
-window.addEventListener("click", function (event) {
+// Adding an Event Listener To The Cloned headers
+window.addEventListener("click",  event => {
+  // Checking if it's a Cloned Header By Checking The ClassName We Set Earlier
   if(event.target.className === "clone"){
     console.log("i am Cloned");
   }
 });
+
+// Adding a Global Event Listener That Intereact To All Headers <h4>
+
+// Global Function
+function GlobalEventListener(type, selector, callback){
+  document.addEventListener(type, e => {
+    // Checking if The Clicked element Matches The Selector
+    if(e.target.matches(selector)) callback();
+  })
+}
+
+GlobalEventListener('click', 'h4', e => {
+    console.log("You Clicked a Header");
+})
