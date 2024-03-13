863831a951f35bc3da254c3d3a11966a7e3e0cd7
amine <aminesdrt42@gmail.com>
2024-03-13 18:03:46 +0100

    Created 'chaining.js' and 'all.js' and 'race.js' Files in 'Promises' Folder

---
+++ Flow/Promises/all.js
@@ -0,0 +1,48 @@
+/* Promise.all(): If you have multiple promises and you want to wait for all of them to resolve, 
+you can use Promise.all(). It takes an array of promises and returns a single promise that resolves 
+when all of the input promises have resolved, or rejects if any of the input promises reject. */
+
+function asyncOperation1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Result of asyncOperation1");
+    }, 2000);
+  });
+}
+
+function asyncOperation2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Result of asyncOperation2");
+    }, 1500);
+  });
+}
+
+function asyncOperation3() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Result of asyncOperation3");
+    }, 1000);
+  });
+}
+
+/* You want to execute all three functions concurrently and collect their results. You can use Promise.all()
+for this purpose: */
+
+const promises = [asyncOperation1(), asyncOperation2(), asyncOperation3()];
+Promise.all(promises)
+  .then((results) => {
+    console.log("All operations completed successfully!");
+    console.log("Results:", results);
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
+
+/* In this example, Promise.all() takes an array of promises as input. It returns a new promise that 
+resolves when all of the input promises have resolved, or rejects if any of the input promises reject.
+Once all three asynchronous operations complete successfully, the .then() callback is executed with an 
+array containing the results of each operation. If any of the operations encounters an error, 
+the .catch() callback will be triggered with the error.
+This allows you to execute multiple asynchronous operations concurrently and handle their results once 
+they all complete. */
---
+++ Flow/Promises/race.js
@@ -0,0 +1,32 @@
+/* Promise.race(): If we have multiple promises and we want to take action as soon as any one of them 
+resolves or rejects, we can use Promise.race(). It takes an array of promises and returns a single promise 
+that resolves or rejects as soon as one of the input promises resolves or rejects. */
+
+function asyncOperation1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Result of asyncOperation1");
+    }, 2000);
+  });
+}
+
+function asyncOperation2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Result of asyncOperation2");
+    }, 1000);
+  });
+}
+
+/* we want to execute both functions but only want to use the result of the one that completes first. 
+we can use Promise.race() for this purpose: */
+
+const myPromises = [asyncOperation1(), asyncOperation2()]
+Promise.race(myPromises)
+  .then((result) => {
+    console.log("The first operation completed successfully!");
+    console.log("Result:", result);
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
